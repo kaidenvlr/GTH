41c8fa6145c99706a1e2306f63051b20c7e88ad5
kaidenvlr <jzxcvblessed@gmail.com>
2022-12-13 11:39:26 +0600

    commit

---
+++ services.php
@@ -0,0 +1,380 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Green Tech Hub</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/ticker-style.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    <!-- <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- Preloader Start -->
+
+    <header>
+        <!-- Header Start -->
+        <div class="header-area">
+            <div class="main-header ">
+                <div class="header-top black-bg d-none d-md-block">
+                    <div class="container">
+                        <div class="col-xl-12">
+                            <div class="row d-flex justify-content-between align-items-center">
+                                <div class="header-info-left">
+                                    <ul>
+                                        <li>
+                                            <script
+                                                src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+                                            <script
+                                                src="//api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=ru-RU"
+                                                type="text/javascript"></script>
+                                            <div><span id="user-city"></span></div>
+                                        </li>
+
+                                        <li><script language="javascript" type="text/javascript">
+                                            var d = new Date();
+                                            
+                                            var day=new Array("Воскресенье","Понедельник","Вторник",
+                                            "Среда","Четверг","Пятница","Суббота");
+                                            
+                                            var month=new Array("января","февраля","марта","апреля","мая","июня",
+                                            "июля","августа","сентября","октября","ноября","декабря");
+                                            
+                                            document.write(day[d.getDay()]+" " +d.getDate()+ " " + month[d.getMonth()]
+                                            + " " + d.getFullYear() + " г.");
+                                            //--></script>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="header-info-right">
+                                    <ul class="header-social">
+                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-telegram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="header-mid d-none d-md-block"> 
+                    <div class="container">
+                        <div class="row d-flex align-items-center">
+                            <div class="col-xl-3 col-lg-3 col-md-3">
+                                <div class="logo">
+                                    <a href="index.html"><img src="assets/img/logo/logo.jpg" alt="logo"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>-->
+                <div class="header-bottom header-sticky">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-xl-10 col-lg-10 col-md-12 header-flex">
+                                <!-- sticky -->
+                                <div class="sticky-logo">
+                                    <a href="index.html"><img src="assets/img/logo/logo.jpg" alt="logo"></a>
+                                </div>
+                                <!-- Main-menu -->
+                                <div class="main-menu d-none d-md-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a href="index.html">Главная</a></li>
+                                            <li><a href="about.html">О нас</a></li>
+                                            <li><a href="news.php">Новости</a></li>
+                                            <li><a href="projects.php">Проекты</a></li>
+                                            <li><a href="services.php">Услуги</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div class="col-xl-2 col-lg-2 col-md-4">
+                                <div class="header-right-btn f-right d-none d-lg-block">
+                                    <i class="fas fa-search special-tag"></i>
+                                    <div class="search-box">
+                                        <form action="#">
+                                            <input type="text" placeholder="Поиск">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-md-none"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        <!-- Footer Start-->
+        <div class="footer-area footer-padding fix">
+            <div class="container">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-xl-5 col-lg-5 col-md-7 col-sm-12">
+                        <div class="single-footer-caption">
+                            <div class="single-footer-caption">
+                                <!-- logo -->
+                                <div class="footer-logo">
+                                    <a href="index.html"><img src="assets/img/logo/logo.jpg" alt=""></a>
+                                </div>
+                                <div class="footer-tittle">
+                                    <div class="footer-pera">
+                                        <p>текст-информация в подвале</p>
+                                    </div>
+                                </div>
+                                <!-- social -->
+                                <div class="footer-social">
+                                    <a href="#"><i class="fab fa-twitter"></i></a>
+                                    <a href="#"><i class="fab fa-instagram"></i></a>
+                                    <a href="#"><i class="fab fa-facebook"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4  col-sm-6">
+                        <div class="single-footer-caption mt-60">
+                            <div class="footer-tittle">
+                                <h4>Новостная рассылка</h4>
+                                <p>Подписаться на новости</p>
+                                <!-- Form -->
+                                <div class="footer-form">
+                                    <div id="mc_embed_signup">
+                                        <form target="_blank"
+                                            action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                                            method="get" class="subscribe_form relative mail_part">
+                                            <input type="email" name="email" id="newsletter-form-email"
+                                                placeholder="Email Address" class="placeholder hide-on-focus"
+                                                onfocus="this.placeholder = ''"
+                                                onblur="this.placeholder = ' Email Address '">
+                                            <div class="form-icon">
+                                                <button type="submit" name="submit" id="newsletter-submit"
+                                                    class="email_icon newsletter-submit button-contactForm"><img
+                                                        src="assets/img/logo/form-iocn.png" alt=""></button>
+                                            </div>
+                                            <div class="mt-10 info"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-5 col-sm-6">
+                        <div class="single-footer-caption mb-50 mt-60">
+                            <div class="footer-tittle">
+                                <h4>Контакты</h4>
+                            </div>
+                            <div>
+                                lorem ipsum
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer-bottom aera -->
+        <div class="footer-bottom-area">
+            <div class="container">
+                <div class="footer-border">
+                    <div class="row d-flex align-items-center justify-content-between">
+                        <div class="col-lg-6">
+                            <div class="footer-copy-right">
+                                <p>
+                                    Copyright &copy;
+                                    <script>document.write(new Date().getFullYear());</script> Все права
+                                    зарегистрированы | Выполнено <a href="https://ecomeken.kz"
+                                        target="_blank">Ecomeken</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="footer-menu f-right">
+                                <ul>
+                                    <li><a href="#">Условия использования</a></li>
+                                    <li><a href="#">Политика конфиденциальности</a></li>
+                                    <li><a href="#">Контакты</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End-->
+    </footer>
+
+    <!-- JS here -->
+    <script>
+        window.onload = function () {
+            jQuery("#user-city").text(ymaps.geolocation.city);
+            jQuery("#user-region").text(ymaps.geolocation.region);
+            jQuery("#user-country").text(ymaps.geolocation.country);
+        }
+    </script>
+    <script>
+        console.clear();
+
+        var baseUrl = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/logo-";
+
+        var logos = [];
+        var names = ["google", "intel", "aol", "amazon", "ford", "cocacola", "mcdonalds", "samsung", "microsoft", "youtube", "nike", "fox", "motorola", "ea", "adobe"];
+
+        var companies = $(".company-logo").toArray().map(createCompany);
+        var current;
+
+        var interval = TweenLite.delayedCall(1.5, function () {
+            swapLogo();
+            interval.restart(true);
+        });
+
+        function createCompany(element) {
+
+            var company = {
+                animate: animate,
+                element: element,
+                logo: getLogo()
+            };
+
+            var leave,
+                enter = getImage(company);
+
+            function animate() {
+
+                leave = enter;
+                enter = getImage(company);
+
+                TweenLite.from(enter, 0.75, { autoAlpha: 0, delay: 0.25 });
+                TweenLite.to(leave, 0.75, { autoAlpha: 0, onComplete: removeImage });
+            }
+
+            function removeImage() {
+                element.removeChild(leave);
+            }
+
+            return company;
+        }
+
+        function swapLogo() {
+            var last = current;
+            current = sample(companies.filter(function (company) {
+                return company !== last;
+            }));
+
+            current.logo = getLogo(current);
+            current.animate();
+        }
+
+        function getImage(company) {
+            var image = new Image();
+            image.src = baseUrl + company.logo + ".png";
+            company.element.appendChild(image);
+            return image;
+        }
+
+        function getLogo(company) {
+            if (!logos.length) logos = shuffle(names.splice(0));
+            if (company) names.push(company.logo);
+            return logos.shift();
+        }
+
+        function sample(array) {
+            var index = Math.floor(Math.random() * array.length);
+            return array[index];
+        }
+
+        function shuffle(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                if (window.CP.shouldStopExecution(1)) { break; } if (window.CP.shouldStopExecution(1)) { break; } if (window.CP.shouldStopExecution(1)) { break; } if (window.CP.shouldStopExecution(1)) { break; } if (window.CP.shouldStopExecution(1)) { break; }
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            window.CP.exitedLoop(1);
+            window.CP.exitedLoop(1);
+            window.CP.exitedLoop(1);
+            window.CP.exitedLoop(1);
+            window.CP.exitedLoop(1);
+            return array;
+        }
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/TweenMax.min.js"></script>
+    <!-- All JS Custom Plugins Link Here here -->
+    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/popper.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="./assets/js/owl.carousel.min.js"></script>
+    <script src="./assets/js/slick.min.js"></script>
+    <!-- Date Picker -->
+    <script src="./assets/js/gijgo.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="./assets/js/wow.min.js"></script>
+    <script src="./assets/js/animated.headline.js"></script>
+    <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Breaking New Pluging -->
+    <script src="./assets/js/jquery.ticker.js"></script>
+    <script src="./assets/js/site.js"></script>
+
+    <!-- Scrollup, nice-select, sticky -->
+    <script src="./assets/js/jquery.scrollUp.min.js"></script>
+    <script src="./assets/js/jquery.nice-select.min.js"></script>
+    <script src="./assets/js/jquery.sticky.js"></script>
+
+    <!-- contact js -->
+    <script src="./assets/js/contact.js"></script>
+    <script src="./assets/js/jquery.form.js"></script>
+    <script src="./assets/js/jquery.validate.min.js"></script>
+    <script src="./assets/js/mail-script.js"></script>
+    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="./assets/js/plugins.js"></script>
+    <script src="./assets/js/main.js"></script>
+
+</body>
+
+</html>
